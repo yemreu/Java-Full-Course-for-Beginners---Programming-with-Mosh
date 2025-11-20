7f18f4778dd1ee61a9c902b7efcff5aee76549b6
yemreu <yunusemreuzun@outlook.com>
2025-11-20 22:39:36 +0300

    Variables

---
+++ src/main/java/com/codewithmosh/Main.java
@@ -0,0 +1,9 @@
+package com.codewithmosh;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Variables().main();
+	}
+
+}
---
+++ src/main/java/com/codewithmosh/Variables.java
@@ -0,0 +1,15 @@
+package com.codewithmosh;
+
+public class Variables {
+
+	public void main() {
+		int age = 30;
+		age = 35;
+		System.out.println(age);
+		
+		int myAge = 30;
+		int herAge = myAge;
+		System.out.println(herAge);
+	}
+	
+}
