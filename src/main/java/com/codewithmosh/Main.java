725dc669606c679eb92db048f7f921108a42e4ca
yemreu <yunusemreuzun@outlook.com>
2025-11-20 23:08:10 +0300

    Primitive Types

--- src/main/java/com/codewithmosh/Main.java
+++ src/main/java/com/codewithmosh/Main.java
@@ -3,7 +3,10 @@ package com.codewithmosh;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Variables");
 		new Variables().main();
+		System.out.println("_________\n\nPrimitive Types");
+		new PrimitiveTypes().main();
 	}
 
 }
---
+++ src/main/java/com/codewithmosh/PrimitiveTypes.java
@@ -0,0 +1,17 @@
+package com.codewithmosh;
+
+public class PrimitiveTypes {
+
+	public void main() {
+		byte age = 30;
+		long viewsCount = 3_123_456_789L;
+		float price = 10.99f;
+		char letter = 'A';
+		boolean isEligible = false;
+		System.out.println(age);
+		System.out.println(viewsCount);
+		System.out.println(price);
+		System.out.println(letter);
+		System.out.println(isEligible);
+	}
+}
